3c7b1b6a1d67eb20e33007d00aaa3b84ef639d64
saish0 <patrickvalera50@yahoo.com>
2022-05-11 16:46:04 -0700

    fix?

--- emitter/index.js
+++ emitter/index.js
@@ -1,9 +1,9 @@
 const sdk = require('node-appwrite');
 const client = new sdk.Client()
 client
-    .setEndpoint('http://localhost:5001/v1') // Your API Endpoint
-    .setProject('626ff525dcb106ce6c56') // Your project ID
-    .setKey('aae7f1661abb80c1a12586a6df90217c487078a87f121bc3a107b7f99197819e01b4d4b66a4b5250bb10cd80f5633ab3c2c0cd2a6f2374839bd3ef9fe7c79a0eceb7c54d87a7e5199c94507dc1d2874214a6cc624221047052a5b9f1da421c54a153a0071c8d148c0a9bfbbbd508e85c117809d466fa6f29b802a3de8284328c')
+    .setEndpoint('http://147.182.235.212/v1') // Your API Endpoint
+    .setProject('stock-sim') // Your project ID
+    .setKey('ec1474839b98e9142acc1683b31627d912ac774c7b2bb24ad858ffff928e987e8b3880d6bce5c795ffcc8bda1a1492cfa7eb6c2040fd647e1f7c111a4ec7b277bb5f13b5dade9fa7f23921774b5bb9449400ada0a42f41e7d26541995d34fb97094efbcb6279f9b32fabd13610450dc82c420beb4d2db4dd0ce4e0932c5dbb52')
     ;
 const database = new sdk.Database(client)
 
@@ -13,24 +13,45 @@ const database = new sdk.Database(client)
 //     console.log(res)
 // })
 const startEmitters = async () => {
-    const {documents} = await database.listDocuments('stocks').then(res=>{return res})
-    for (let i=0;i<documents.length;i++){
-        startEmitter(documents[i].$id)
+    // Fetch All Stocks
+    const { documents } = await database.listDocuments('stock').then(res => { return res })
+    // Start Emitter fore each stock, updating prices
+    for (let i = 0; i < documents.length; i++) {
+        startEmitter(documents[i])
     }
-    console.log(documents)
-
 }
-const startEmitter = async (id) => {
+
+const startEmitter = async (stock) => {
     let i = 0
     while (1) {
-        update(id)
+        update(stock)
         await delay(2000)
         i += 1
     }
 }
-const update = async (id) => {
-    let promise = await database.updateDocument('stocks', id, {
-        currentPrice: Math.floor(Math.random() * 10),
+const update = async (stock) => {
+    let ar
+    let newPrice
+    let prom = await database.getDocument('stock', stock.$id).then(res => {
+        let percentChange = Math.floor(Math.random() * 4) + 1
+        percentChange *= Math.round(Math.random()) ? 1 : -1
+        newPrice = res.currentPrice + (res.currentPrice * (percentChange / 100))
+
+
+        // Append new value to price history
+        ar = res.priceHistory
+        if (ar.length < 1800) {
+            ar = ar.slice(0)
+        } else {
+            ar = ar.slice(1)
+        }
+        ar.push(Number(newPrice.toFixed(2)))
+    })
+    // Generate Percent Change +/-
+
+    let promise = await database.updateDocument('stock', stock.$id, {
+        currentPrice: newPrice,
+        priceHistory: ar
     }).then((res) => {
         console.log('updated')
     })
@@ -38,9 +59,9 @@ const update = async (id) => {
 }
 
 const delay = async (ms) => {
-    wait=Math.ceil(Math.random()*3000)
+    wait = Math.ceil(Math.random() * 3000)
     // wait=200
-    return new Promise(resolve => setTimeout(resolve,wait));
+    return new Promise(resolve => setTimeout(resolve, wait));
 }
 
 startEmitters()
