318ef6b6d7a17444068468dcb229b07950f670a4
saish0 <patrickvalera50@yahoo.com>
2022-05-12 20:15:33 -0700

    fix links | add menu

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Avatar, Box, Button, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import FlexBox from './utilcomps/FlexBox'
 import api from '../api'
@@ -8,14 +8,14 @@ import { RiStockLine } from 'react-icons/ri'
 const Header = () => {
   const [user, setUser] = useState()
   const router = useRouter()
-
-  const handleLogout = () => {
-    api.account.deleteSession('current');
-    setUser()
-    router.push('/userlogin')
-  }
-
-
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const fetchUser = async () => {
     try {
       let user = await api.account.get()
@@ -24,11 +24,16 @@ const Header = () => {
       console.log(e)
     }
   }
+  const handleLogout = () => {
+    api.account.deleteSession('current');
+    setUser()
+    router.push('/userlogin')
+  }
   useEffect(() => {
     fetchUser()
   }, [api])
   return (
-    <FlexBox sx={{ p: {xs:1,md:2}, width: '100%', bgcolor: 'background.paper', mb: {xs:1,md:4},  }}>
+    <FlexBox sx={{ p: { xs: 1, md: 2 }, width: '100%', bgcolor: 'background.paper', mb: { xs: 1, md: 4 }, }}>
       <FlexBox sx={{ justifyContent: 'left', flexGrow: '1' }}>
         <Link href='/'>
           <Typography variant='h5' sx={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}><RiStockLine size='2.2rem' style={{ color: 'green' }} />Mock Trade</Typography>
@@ -36,9 +41,48 @@ const Header = () => {
       </FlexBox>
       <FlexBox sx={{ justifyContent: 'center' }}>
         <Button href='/leaderboard'>LeaderBoard</Button>
-        {user ?
-          <Button onClick={handleLogout}>Logout</Button> :
-          <Button href='/userlogin' variant='contained'>Login</Button>
+        {!user ?
+          <Button href='/userlogin' variant='contained'>Login</Button> :
+          <>
+            <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+              <Tooltip title="Account settings">
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  sx={{ ml: 2 }}
+                  aria-controls={open ? 'account-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                >
+                  <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                </IconButton>
+              </Tooltip>
+            </Box>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem>
+                Profile
+              </MenuItem>
+              <MenuItem>
+                Settings
+              </MenuItem>
+              <MenuItem onClick={handleLogout}>
+                Logout
+              </MenuItem>
+
+            </Menu>
+          </>
         }
 
       </FlexBox>
